70355f3dde2ad00290b4e4f10842e04707acd263
“TranNam” <anhnam.2k.yn@gmail.com>
2024-05-07 09:22:50 +0700

    add ui settings

--- app/src/main/java/com/example/scannerqr/ui/setting/SettingsAdapter.kt
+++ app/src/main/java/com/example/scannerqr/ui/setting/SettingsAdapter.kt
@@ -1,22 +1,68 @@
 package com.example.scannerqr.ui.setting
 
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
+import com.bumptech.glide.Glide
 import com.example.scannerqr.model.Settings
 import com.example.socialmedia.base.recyclerview.BaseRecyclerAdapter
 import com.example.socialmedia.base.recyclerview.BaseViewHolder
 import com.scan.scannerqr.R
+import com.scan.scannerqr.databinding.ItemSettingContentBinding
+import com.scan.scannerqr.databinding.ItemSettingSwitchBinding
+import com.scan.scannerqr.databinding.ItemSettingsBinding
 
 class SettingsAdapter : BaseRecyclerAdapter<Settings, SettingsAdapter.SettingsViewHolder>() {
-inner class SettingsViewHolder(val binding: ViewDataBinding) : BaseViewHolder<Settings>(binding){
+    inner class SettingsViewHolder(val binding: ViewDataBinding) :
+        BaseViewHolder<Settings>(binding) {
+        override fun bind(itemData: Settings?) {
+            super.bind(itemData)
+            if (binding is ItemSettingContentBinding) {
+                binding.title.text = itemData?.title
+                binding.content.text = itemData?.description
+            }
+            if (binding is ItemSettingsBinding) {
+                binding.title.text = itemData?.title
+              Glide.with(itemView).load(itemData?.icon).into(binding.imageView)
+            }
+            if (binding is ItemSettingSwitchBinding) {
+                binding.title.text = itemData?.title
+                binding.content.text = itemData?.description
 
-}
+                binding.btnSwitch.visibility =
+                    if (itemData?.showButtonSwitch == true) View.VISIBLE else View.GONE
+                binding.btnSwitch.isEnabled = itemData?.switchEnabled ?: false
+            }
+        }
+    }
 
     override fun getItemLayoutResource(viewType: Int): Int {
-        return  R.layout.item_settings
+        return when (viewType) {
+            TYPE_CONTENT -> R.layout.item_setting_content
+            TYPE_CONTENT_SWITCH -> R.layout.item_setting_switch
+            else -> R.layout.item_settings
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = listItem[position]
+        return if (item.description.isNotEmpty() && !item.showButtonSwitch) {
+            TYPE_CONTENT
+        } else if (item.showButtonSwitch) {
+            TYPE_CONTENT_SWITCH
+        } else if (item.title.isNotEmpty()) {
+            TYPE_HEADER
+        }else 4
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SettingsViewHolder {
-       return SettingsViewHolder(getViewHolderDataBinding(parent, viewType))
+        return SettingsViewHolder(getViewHolderDataBinding(parent, viewType))
+    }
+
+    companion object {
+        const val TYPE_HEADER = 0
+        const val TYPE_CONTENT = 1
+        const val TYPE_CONTENT_SWITCH = 3
     }
 }
--- app/src/main/java/com/example/scannerqr/ui/setting/SettingsFragment.kt
+++ app/src/main/java/com/example/scannerqr/ui/setting/SettingsFragment.kt
@@ -15,7 +15,7 @@ class SettingsFragment : BaseFragmentWithBinding<FragmentSettingsBinding>() {
 
     private val viewModel: SettingsViewModel by viewModels()
     private lateinit var adapter: SettingsAdapter
-    private val listSettings : ArrayList<Settings> = arrayListOf()
+    private val listSettings: ArrayList<Settings> = arrayListOf()
 
 
     override fun getViewBinding(inflater: LayoutInflater): FragmentSettingsBinding {
@@ -30,9 +30,62 @@ class SettingsFragment : BaseFragmentWithBinding<FragmentSettingsBinding>() {
     }
 
     override fun initData() {
+        listSettings.add(Settings("General", R.drawable.ic_general))
     }
 
     override fun initAction() {
+        initDataSettings()
+    }
 
+    fun initDataSettings() {
+        listSettings.add(Settings(title = "General", icon = R.drawable.ic_general))
+        listSettings.add(Settings(title = "Display", description = "System default options"))
+        listSettings.add(Settings(title = "Help and Feedback"))
+        listSettings.add(Settings(title = "Scan control", icon = R.drawable.ic_control))
+        listSettings.add(Settings(title = "Open web pages automatically", showButtonSwitch = true))
+        listSettings.add(
+            Settings(
+                title = "Scan continuously",
+                description = "Only add scans to history",
+                showButtonSwitch = true
+            )
+        )
+        listSettings.add(
+            Settings(
+                title = "Duplicate barcodes",
+                description = "Save duplicate barcodes in history",
+                switchEnabled = true,
+                showButtonSwitch = true
+            )
+        )
+        listSettings.add(
+            Settings(
+                title = "Confirm manual scan",
+                description = "Avoid accidental scanning",
+                showButtonSwitch = true
+            )
+        )
+        listSettings.add(Settings(title = "Sound", showButtonSwitch = true))
+        listSettings.add(Settings(title = "Copy to clipboard", showButtonSwitch = true))
+        listSettings.add(Settings(title = "Product barcode", icon = R.drawable.ic_settings_barcode))
+        listSettings.add(
+            Settings(
+                title = "Country to search for products",
+                description = "System default options"
+            )
+        )
+        listSettings.add(
+            Settings(
+                title = "Product information",
+                description = "Display product information and price if available ",
+                showButtonSwitch = true
+            )
+        )
+        listSettings.add(Settings(title ="Introduce", icon = R.drawable.ic_introduce))
+        listSettings.add(Settings(title = "Introduce"))
+        listSettings.add(Settings(title = "Open source license"))
+        listSettings.add(Settings(title = "Privacy policy"))
+        listSettings.add(Settings(title = "App version", description = "Version 1.1"))
+        adapter.submitList(listSettings)
     }
 }
